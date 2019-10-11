c2141008591743f2deb18813f776b74978cf9cd6
chetan <chetankudaravalli16@gmail.com>
2019-10-11 13:19:43 -0500

    Added area calculator

--- components/adder/adder.js
+++ components/adder/adder.js
@@ -1,17 +1,17 @@
-const add = (x, y) => { return x + y }
+const add = (x) => { return   Math.PI * x * x }
 
 const adder = new Vue({
   el: '#adder',
   data: {
-    guest: 'Emmett',
+    
     firstNumber: 5,
-    secondNumber: 3
+  
   },
   computed: {
     result: function () {
       const i = parseInt(this.firstNumber)
-      const j = parseInt(this.secondNumber)
-      return `${this.guest}, your sum is ${add(i, j)}.`
+      
+      return ` your area is ${add(i)}.`
     }
   }
 })
